d99add8e71362a65d3d099df36ba4a54e44bfb44
nineten <jayden87@gmail.com>
2018-05-28 17:33:37 +0800

    chore: Rename to main view controller

--- CDGLayoutTricks/ViewControllers/MainViewController.swift
+++ CDGLayoutTricks/ViewControllers/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  CDGLayoutTricks
 //
 //  Created by Jayden Ma on 28/5/18.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
